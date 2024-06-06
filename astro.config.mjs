54ad53114c9adf1fb1b37f7e98712b6ab0b7e372
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2024-06-06 16:54:28 +0200

    development mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   site: site,
   base: 'academy',
   image: {
-     service: passthroughImageService(),
+    service: passthroughImageService(),
   },
   integrations: [starlight({
     title: 'Patchstack Academy',
