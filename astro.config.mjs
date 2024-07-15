9c769e0cd711ce29f4a25fc9d8be3292909a7c6d
Maciek Palmowski <m.palmowski@freshpixels.pl>
2024-07-15 12:13:45 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ const site = site_url || 'http://localhost:4321';
 // https://astro.build/config
 export default defineConfig({
   site: site,
-  base: 'academy',
+  base: 'test-academy',
   image: {
     service: passthroughImageService(),
   },
